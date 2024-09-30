df41245454393734914a746fde3244585022bc78
wubbie88 <gallaher.emily@yahoo.com>
2024-09-29 22:58:39 -0500

    add search and event listeners

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css"
+
 export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+
+    function search (event)  {
+        event.preventDefault();
+        alert("Searching...")
+}
+    function handleKeywordChange(event) {
+
+    }
+
+
     return (
         <div className="Dictionary">
-            Hello from Dictionary
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange} />
+            </form>
         </div>
     )
 }
