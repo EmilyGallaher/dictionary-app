2b0cbbb0f4752e47eaecfa04faca4b1e2bd36275
wubbie88 <gallaher.emily@yahoo.com>
2024-09-30 21:04:54 -0500

    implement api to turn definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
-import "./Dictionary.css"
+import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
-    handleResponse(response);    {
-
-    }
+    let [results, setResults] = useState({});
 
     function search (event)  {
         event.preventDefault();
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
 }
-
+    function handleResponse(response)   {
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
+    }
  
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
@@ -25,6 +27,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
             </form>
+            <Results />
         </div>
     )
 }
