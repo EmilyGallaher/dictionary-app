facecce2b95761e3edafa06e3db37458c060fa90
wubbie88 <gallaher.emily@yahoo.com>
2024-10-02 22:36:45 -0500

    results not showing

--- src/Phoenetic.js
+++ src/Phoenetic.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-export default function Phoenetic(props) {
-    return (
-        <div className="Phoenetic">
-            <a href={props.phoenetic.audio} target="_blank" rel="noreferrer">
-                Listen
-            </a>
-            <br />
-            {props.phoenetic.text}
-        </div>
-    );
+export default function Phonetic(props) {
+  console.log(props.phonetic);
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
 }
