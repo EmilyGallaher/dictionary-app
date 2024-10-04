d579b07d63a49c56a61f2822e116e81ad0fcb5c2
wubbie88 <gallaher.emily@yahoo.com>
2024-10-03 22:03:07 -0500

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,6 @@
 import React from "react";
+import "./Meaning.css";
 import Synonyms from "./Synonyms";
-import "./Meaning.css"
 
 export default function Meaning(props) {
   return (
@@ -9,13 +9,10 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-              <div className="definition">
-              {definition.definition}
-              </div>
-              <div className="example">
-              {definition.example}
-              </div>
-              <Synonyms synonyms={definition.synonyms} />
+            <div className="definition">{definition.definition}</div>
+
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
