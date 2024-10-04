d54f25325897b8a3a1d5c54fa8ee63f2a35314f0
wubbie88 <gallaher.emily@yahoo.com>
2024-10-03 19:57:53 -0500

    adding css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,29 +6,44 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search()  {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
+  if (loaded)   {
   return (
     <div className="Dictionary">
-      <form onSubmit={search}>
+      <section>
+      <form onSubmit={handleSubmit}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <div className="hint">
+        Suggested words: sunset, wine, cheese...
+        </div>
+      </section>
       <Results results={results} />
     </div>
   );
+}
+} else {
+    load();
+    return(null);
 }
